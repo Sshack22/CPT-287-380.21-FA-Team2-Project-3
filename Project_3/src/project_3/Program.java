4d250a33a8ffe503adaa9b1dfa2dffc353d2ee64
lukeahunt91 <91393041+lukeahunt91@users.noreply.github.com>
2021-11-14 18:54:54 -0600

    Reformatted and Added Comments
    @Sshack22 Also, closed file and changed error throw.

--- Project_3/src/project_3/Program.java
+++ Project_3/src/project_3/Program.java
@@ -1,29 +1,32 @@
 package project_3;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
 
-
-	public static void main(String[] args) throws FileNotFoundException {
-
+	public static void main(String[] args) throws IOException {
+		 
 		FileInputStream inputFile = new FileInputStream("Morse_Code.txt");
-		
 		Scanner scanner = new Scanner(inputFile);	// Input file scanner
-
-		TreeNode root = new TreeNode(' ');
-
-		while (scanner.hasNext()) {
-
-			String token = scanner.nextLine();
-			
-			Methods.builder(token, root);
-
-		}
 		
-		System.out.print(root.right.left.val);
-		scanner.close();
+		//Create Tree
+		TreeNode root = new TreeNode(' '); // Create node for root of morse code tree
+		
+		while (scanner.hasNext()) { // Loop through all lines of text file
+			String token = scanner.nextLine(); // Store line of text
+			Methods.builder(token, root); // Create node for current line
+		} // End while
+		
+		//TODO: Create loop for user input
+		
+		//TODO: Read user input and determine if decode or encode
+		
+		//TODO: Remove debug code
+		//BinaryTreeAlgorithms.preorderTraverse(root);
+		
+		inputFile.close(); // Close file
+		scanner.close(); // Close scanner
 	}
 }
